bbde55fb702ef355423795b7723ca671b175e0d5
tommie1236 <timooosterom@gmail.com>
2022-09-10 13:13:50 +0200

    fixed a few functions

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-# 'V' after a function means its finished
-
 # imports
 import os
 import shutil
@@ -12,159 +10,245 @@ MINECRAFT_FOLDER = os.path.expanduser('~/.minecraft')
 # code
 class config():
 
-	def get_versions(modloader=None): # V
-		
+	def get_profiles(modloader=None, version=None): # done but could probably be optimised/shrinked
 		"""
-		returns all the registered versions
-		only returns mods from a specific modloader if modloader is specified
+		returns a dict of all registered profiles that match to
+		the modloader, version if specified
+		parameters: modloader, version  #both are set to None when not specified
+		returns: profiles (list)
+				 None when None exists
 		"""
 		config = configparser.ConfigParser()
 		config.read('config.ini')
-		if modloader == None:
-			version_names = dict(config.items('versions'))
-			versions = list(version_names.keys())
-		else:
-			for key, value in config.items('versions'):
-				if key.split('_')[0] == modloader:
-					version_names = dict()
-					versions = list(version_names.keys())
-				else:
-					return False
-		return version_names, versions
+		profiles = dict(config.items('profiles'))
+		if modloader == None and version == None: # if none are specified
+			return profiles
+		newprofiles = {} # setup empty dict for the other possibilities
+		if modloader != None and version == None: # if only the modloader is specified
+			for key, value in profiles.items():
+				if value.split(' ')[0] == modloader:
+					newprofiles[key] = value
+			if newprofiles != {}:
+				return newprofiles
+			else:
+				return None
+		elif modloader == None and version != None: # if only the version is specified
+			for key, value in profiles.items():
+				if value.split(' ')[1] == version:
+					newprofiles[key] = value
+			if newprofiles != {}:
+				return newprofiles
+			else:
+				return None
+		else: # if both are specified
+			for key, value in profiles.items():
+				if value.split(' ')[0] == modloader and value.split(' ')[1] == version:
+					newprofiles[key] = value
+			if newprofiles != {}:
+				return newprofiles
+			else:
+				return None
 
-	def add_version(version, modloader): # V
+	def add_profile(profile_name: str, version: str, modloader: str): # Done
 		"""
-		registers specified version as new versions
-		arguments: version, modLoader
+		registers specified profile as a new profile
+		parameters: profile_name, version, modloader
 		return: True when succesfull
 				False when the specified version already exists
 		"""
 		config = configparser.ConfigParser()
 		config.read('config.ini')
-		if version in config.items():
+		if profile_name in toSingleStrKey(dict(config.items('profiles'))).keys():
 			return False
 		else:
-			key = f'{modloader}_{version}'
-			value = f'{modloader} mods {version}'
-			config.set('versions', key, value)
-			with open(r'config.ini', 'w') as configfile:
-				config.write(configfile)
+			key = toDoubleStr(profile_name)
+			value = f'{modloader} {version}'
+			config.set('profiles', key, value)
+			with open(r'config.ini', 'w') as configFile:
+				config.write(configFile)
+			return True
+		pass
+
+	def rm_profile(profile_name: str): # Docsting
+		key = profile_name
+		config = configparser.ConfigParser()
+		config.read('config.ini')
+		config.remove_option('profiles', key)
+		with open(r'config.ini', 'w') as configfile:
+			config.write(configfile)
 			return True
 
-	def rm_version(version, modloader):
+	def get_modloader(profile_name: str): # Done
 		"""
-		remove the specified version and the mods
-		argumwnts: version, modloader
-		return: True when succesful
-				False when tge version doesn't exist
+		get the modloader of the selected profile
+		parameters: profile_name
+		returns: (str) the modloader of the profile
 		"""
-# -----------------------------------------------
-		confirm = input(f'Are you sure you want to delete the mods for {modloader} {version}? (y/n) ')
-		if confirm == 'y' or confirm == 'yes':
-			versions.remove(version, modloader) # ------------
-			value = f'{modloader}_{version}'
-			config = configparser.ConfigParser()
-			config.read('config.ini')
-			config.remove_option('versions', value)
-			with open(r'config.ini', 'w') as configfile:
-				config.write(configfile)
-			print(f'removed version {value}')
-		elif confirm == 'n' or confirm == 'no':
-			print('deletion canceled')
+		config = configparser.ConfigParser()
+		config.read('config.ini')
+		profile_names = toSingleStrKey(dict(config.items('profiles')))
+		print(profile_names[profile_name].split(' ')[0])
 
-class version():
+	def get_version(profile_name: str):	# Done
+		"""
+		get the version of the selected profile
+		parameters: profile_name
+		returns: (str) the version of the profile
+		"""
+		config = configparser.ConfigParser()
+		config.read('config.ini')
+		profile_names = toSingleStrKey(dict(config.items('profiles')))
+		print(profile_names[profile_name].split(' ')[1])
 
-	def get_current(): # V
-		with open(f'{MINECRAFT_FOLDER}/mods/version.txt', 'r') as vf:
-			return vf.read().strip()
+
+class profile():
+
+	def get_current(): # Done
+		"""
+		Gets the current installed profile name, version and modloader
+		Parameters: None
+		Returns: profile_name (str)
+				 version (str)
+				 modloader (str)
+		"""
+		with open(f'{MINECRAFT_FOLDER}/mods/profile.txt', 'r') as pf:
+			profile_name, version, modloader = pf.read().strip()
+		return toSingleStr(profile_name), version, modloader
 
 # implement version switching
 # -----------------------------------------------
-	def switch_to(version, modloader):
-		print(f'current version: {mods.get_current_version()}')
-		if version == mods.get_current_version():
-			print(f'mods are already for {version}')
-			return False
-		else:
-			for mod in mods.get_all(version, modloader):
-				pass
-			print(f'switching to: {version}')
-# -----------------------------------------------
-	
-	def add(version, modloader): # V
-		if os.path.isdir(f'{MINECRAFT_FOLDER}/{modloader} mods {version}') == False:
-			if config.add_version(version, modloader) == True:
-				os.mkdir(f'{MINECRAFT_FOLDER}/{modloader} mods {version}')
-				print(f'added mods version: {modloader} {value}')
-				return True
-			else:
-				print(f'{modloader} {version} already exists')
-				return False
-		else:
-			return False
-
-	def remove(version, modloader):
+	def switch_to(version, modloader): # --------------------
+		
 		pass
 
+		# print(f'current version: {mods.get_current_version()}')
+		# if version == mods.get_current_version():
+		# 	print(f'mods are already for {version}')
+		# 	return False
+		# else:
+		# 	for mod in mods.get_all(version, modloader):
+		# 		pass
+		# 	print(f'switching to: {version}')
+# -----------------------------------------------
+	
+	def add(profile_name: str, version: str, modloader: str): # Done
+		"""
+		adds a new profile
+		parameters:
+			profile_name (str)
+			version (str)
+			modloader (str)
+		returns:
+			True when succesfull
+			False when the profile already exists
+		"""
+		if os.path.isdir(f'{MINECRAFT_FOLDER}/mod-profiles/{profile_name}') == False and \
+			config.add_profile(profile_name, version, modloader) == True:
+			os.mkdir(f'{MINECRAFT_FOLDER}/mod-profiles/{profile_name}')
+			return True
+		else:
+			# gui.promt('version already exists')
+			return False
 
-class mods():
+	def remove(profile_name: str): # Done
+		# ----------
+		# gui confirm promt
+		# ----------
+		if confirm == True:
+			if config.rm_profile(profile_name) == True:
+				shutil.rmtree(f'{MINECRAFT_FOLDER}/mod-profiles/{profile_name}')
+		return True
 
-	def get_all(version, modloader): # V
+
+
+
+class mods(): 
+
+	def get_all(profile_name: str): # Done
+		"""
+		gets all mod names of the specified profile
+		parameters:
+			profile_name (str)
+		returns
+			mods (list)
+		"""
 		mods = []
-		for mod in os.listdir(f'{MINECRAFT_FOLDER}/{modloader} mods {version}'):
+		for mod in os.listdir(f'{MINECRAFT_FOLDER}/mod-profiles/{profile_name}'):
 			mods.append(mod)
 		return mods
 
 
-	def add(version, modloader, mod_file, mod_name): # V
+	def add(profile_name: str, mod: dict): # Done
 		"""
-		adds a mod to a existing version, if that version doesn't exists it will be created
-		arguments: 
-			version: the version the mod is made for
-			modloader: the modloader the mod is made for (fabric, forge)
-			mod_file: the modfile thats added
-			mod_name: the name of the modfile
+		adds a mod to a existing version 
+		parameters: 
+			profile_name (str)
+			mod (dict)
 		returns:
 			True: when succesfull
-			False: when unsuccesfull
+			False: when version doesn't exist
 		"""
-		if os.path.isdir(f'{MINECRAFT_FOLDER}/{modloader} mods {version}'):
-			shutil.copy2(mod_file, f'{MINECRAFT_FOLDER}/{modloader} mods {version}/{mod_name}')
-			return True
+		version = config.get_version(profile_name)
+		modloader = config.get_version(profile_name)
+
+		if version in config.get_profiles(modloader=config.get_modloader(profile_name), version=config.get_version(profile_name)) and \
+			os.path.isdir(f'{MINECRAFT_FOLDER}/mod-profiles/{profile_name}'):
+			if mod[version] == version and mod[modloader] == modloader:
+				shutil.copy2(mod[file], f'{MINECRAFT_FOLDER}/mod-profiles/{profile_name}/{mod[name]}')
+				return True
+			else:
+				# gui.prompt('this mod is not compatible with this version')
+				return False
 		else:
-			print(f'Error: failed to add {mod_file} to {modloader} {version}') 
+			# gui.prompt('this version doesn't exist')
 			return False
 	
-	def remove(version, modloader, mod_name): # V
+	def remove(profile_name, mod): # Done
 		"""
-		removes a mod from the specified version.
-		arguments: 
-			version: the version the mod is in
-			modloader: the modloader the mod is in (fabric, forge)
-			mod_name: the name of the mod removed
+		removes a mod from the specified profile.
+		parameters: 
+			profile_name (str)
+			mod (dict)
 		returns:
 			True: when succesfull
 			False: when unsuccesfull
 		"""
 		try:
-			os.remove(f'{MINECRAFT_FOLDER}/{modloader} mods {version}/{mod_name}')
+			os.remove(f'{MINECRAFT_FOLDER}/mod-profiles/{profile_name}/{mod[name]}')
+			# Confirm promt?
 			return True
 		except:
 			return False
 
-def main(): 
-	
-	# below are options, THEY ARE NOT ALL UPDATED!
-	
+
+def toDoubleStr(string): # Done
+	return f'"{string}"'
+
+def toSingleStr(doubleString): # Done
+	return doubleString[1:-1]
+
+def toSingleStrKey(dictionary): # Done
+	newdict = {}
+	for key, value in dictionary.items():
+		newkey = toSingleStr(key)
+		newdict[newkey] = value
+	return newdict
+
+
+
+def main():
 	# version_names, versions = config.get_versions('fabric')
 	# mods.get_all('1.18.2', 'fabric')
 	# print(f'{version_names},{versions}')
 	# print(cversion)
 	# mods.switch_to('1.18.2')
 	# config.add_version('1.18.1', 'fabric')
-	# config.rm_version('1.18.2')
+	# config.rm_profile('1.18.2')
 	# print(version.get_current())
-	# mods.add()
+	# config.get_modloader('fabric mods 1.18.1')
+	# print(toSingleStr('"hi"'))
+	# print(config.get_profiles(version='1.18.1', modloader='fabric'))
+	# config.add_profile('fabric mods 1.19', '1.19', 'fabric')
 	pass
 
 if __name__ == '__main__':
@@ -173,11 +257,3 @@ if __name__ == '__main__':
 	else:
 		print('This software only works on Linux systems')
 
-
-# TASKS
-# - implement version switching
-# V implement adding mods 
-# V implement removing mods
-# - make a gui
-# V move the folder logic from config.add_version() to version.add()
-# - move the folder logic from config.rm_version() to version.remove()
