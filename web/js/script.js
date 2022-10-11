e979262a79cc114b08c94cc727e367f8e9ee363c
tommie1236 <timooosterom@gmail.com>
2022-10-11 20:31:58 +0200

    implemented gui design

---
+++ web/js/script.js
@@ -0,0 +1,11 @@
+/*const fileInput = document.getElementById('file-input');
+fileInput.addEventListener('change', (event) => {
+	const file = event.target.files; // uploaded file
+})*/
+
+function selectVersion() {
+	console.log('version');
+};
+function selectModloader() {
+	console.log('modloader');
+};
